9d561268fbef07471748d3d253c86aad5f0b0ec5
Fernando <fernan.hdez2015@gmail.com>
2024-06-03 07:57:47 -0300

    Settings. Include the urls auth_app in main urls

--- cine/auth_user/urls.py
+++ cine/auth_user/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import include, path
+from rest_framework import routers
+from auth_user import views
+
+router = routers.DefaultRouter()
+# router.register(r'client', views.CustumerViewSet)
 
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include("auth_user.urls"))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- cine/cine/settings.py
+++ cine/cine/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External app
+    'rest_framework',
+
+    # Local app
+    'auth_user',
+    'movies'
 ]
 
 MIDDLEWARE = [
